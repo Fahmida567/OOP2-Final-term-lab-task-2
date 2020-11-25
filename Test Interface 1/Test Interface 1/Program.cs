490d38e083e097beb6c0541bb5f160288a3c4fcc
Fahmida <fahmidaturin34@gmail.com>
2020-11-25 10:01:42 +0600

    Lab task not completed yet

---
+++ Test Interface 1/Test Interface 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_Interface_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
